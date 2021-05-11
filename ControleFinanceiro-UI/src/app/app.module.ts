27653f38e89b4b47f82acdbff2b99971bd617252
Luicil <luicilfernandes@hotmail.com>
2021-05-11 20:35:46 -0300

    HTML form Nova Categoria

--- ControleFinanceiro-UI/src/app/app.module.ts
+++ ControleFinanceiro-UI/src/app/app.module.ts
@@ -14,6 +14,12 @@ import {MatTableModule} from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { NovaCategoriaComponent } from './components/Categoria/nova-categoria/nova-categoria.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -28,7 +34,13 @@ import { NovaCategoriaComponent } from './components/Categoria/nova-categoria/no
     MatTableModule,
     MatIconModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatSelectModule
   ],
   providers: [
     TiposService,
