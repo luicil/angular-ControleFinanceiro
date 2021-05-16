6b2a478b027f8fd43ed2d4d82da75711e57fba69
Luicil <luicilfernandes@hotmail.com>
2021-05-16 14:42:46 -0300

    Repositório Genérico

--- ControleFinanceiro.DAL/Interfaces/IRepositorioGenerico.cs
+++ ControleFinanceiro.DAL/Interfaces/IRepositorioGenerico.cs
@@ -1,7 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace ControleFinanceiro.DAL.Interfaces
 {
-    public interface IRepositorioGenerico
+    public interface IRepositorioGenerico<TEntity> where TEntity : class
     {
+        IQueryable<TEntity> PegarTodos();
+        Task<TEntity> PegarPeloID(int id);
+        Task<TEntity> PegarPeloID(string id);
+        Task Inserir(TEntity entity);
+        Task Inserir(List<TEntity> entity);
+        Task Atualizar(TEntity entity);
+        Task Excluir(string id);
+        Task Excluir(int id);
+
     }
 }
--- ControleFinanceiro.DAL/Repositories/RepositorioGenerico.cs
+++ ControleFinanceiro.DAL/Repositories/RepositorioGenerico.cs
@@ -1,10 +1,126 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleFinanceiro.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace ControleFinanceiro.DAL.Repositories
 {
-    public class RepositorioGenerico
+    public class RepositorioGenerico<TEntity> : IRepositorioGenerico<TEntity> where TEntity : class
     {
-        public RepositorioGenerico()
+
+        private readonly Contexto _contexto;
+
+        public RepositorioGenerico(Contexto contexto)
         {
+            _contexto = contexto;
+        }
+
+        public async Task Atualizar(TEntity entity)
+        {
+            try
+            {
+                var registro = _contexto.Set<TEntity>().Update(entity);
+                registro.State = EntityState.Modified;
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Excluir(string id)
+        {
+            try
+            {
+                var entity = await PegarPeloID(id);
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Excluir(int id)
+        {
+            try
+            {
+                var entity = await PegarPeloID(id);
+                _contexto.Set<TEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Inserir(TEntity entity)
+        {
+            try
+            {
+                await _contexto.AddAsync(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Inserir(List<TEntity> entity)
+        {
+            try
+            {
+                await _contexto.AddRangeAsync(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> PegarPeloID(int id)
+        {
+            try
+            {
+                var entity = await _contexto.Set<TEntity>().FindAsync(id);
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> PegarPeloID(string id)
+        {
+            try
+            {
+                var entity = await _contexto.Set<TEntity>().FindAsync(id);
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IQueryable<TEntity> PegarTodos()
+        {
+            try
+            {
+                return _contexto.Set<TEntity>();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
