afec1487a7ea92b37b2356057f9f616c512eabc9
Luicil <luicilfernandes@hotmail.com>
2021-05-18 18:09:51 -0300

    Controller Funcao

---
+++ ControleFinanceiro.API/ControleFinanceiro.API/Controllers/FuncaoController.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL;
+using ControleFinanceiro.DAL.Interfaces;
+using ControleFinanceiro.API.ViewModels;
+
+namespace ControleFinanceiro.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuncaoController : ControllerBase
+    {
+        private readonly IFuncaoRepositorio _funcaoRepositorio;
+
+        public FuncaoController(IFuncaoRepositorio funcaoRepositorio)
+        {
+            _funcaoRepositorio = funcaoRepositorio;
+        }
+
+        // GET: api/Funcao
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Funcao>>> GetFuncoes()
+        {
+            return await _funcaoRepositorio.PegarTodos().ToListAsync();
+        }
+
+        // GET: api/Funcao/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Funcao>> GetFuncao(string id)
+        {
+            var funcao = await _funcaoRepositorio.PegarPeloID(id);
+
+            if (funcao == null)
+            {
+                return NotFound();
+            }
+
+            return funcao;
+        }
+
+        // PUT: api/Funcao/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFuncao(string id, FuncoesViewModel funcoes)
+        {
+            if (id != funcoes.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                Funcao funcao = new Funcao
+                {
+                    Id = funcoes.Id,
+                    Name = funcoes.Name,
+                    Descricao = funcoes.Descricao
+                };
+                await _funcaoRepositorio.AtualizarFuncao(funcao);
+                return Ok(new
+                {
+                    mensagem = $"Função {funcao.Name}  atualizada com sucesso."
+                });
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        // POST: api/Funcao
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Funcao>> PostFuncao(FuncoesViewModel funcoes)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Funcao funcao = new Funcao
+                {
+                    Name = funcoes.Name,
+                    Descricao = funcoes.Descricao
+                };
+                await _funcaoRepositorio.AdicionarFuncao(funcao);
+                return Ok(new
+                {
+                    mensagem = $"Função {funcao.Name} adicionada com sucesso."
+                });
+
+            }
+
+            return BadRequest(ModelState);
+
+        }
+
+        // DELETE: api/Funcao/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Funcao>> DeleteFuncao(string id)
+        {
+            var funcao = await _funcaoRepositorio.PegarPeloID(id);
+            if (funcao == null)
+            {
+                return NotFound();
+            }
+            await _funcaoRepositorio.Excluir(funcao);
+            return Ok(new
+            {
+                mensagem = $"Função {funcao.Name} adicionada com sucesso."
+            });
+        }
+
+    }
+}
---
+++ ControleFinanceiro.API/ControleFinanceiro.API/ViewModels/FuncoesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ControleFinanceiro.API.ViewModels
+{
+    public class FuncoesViewModel
+    {
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Descricao { get; set; }
+
+    }
+}
