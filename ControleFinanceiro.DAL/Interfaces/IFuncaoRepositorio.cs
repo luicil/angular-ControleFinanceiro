07f784f0328e0dfbb51963ca6a070b3af1059442
Luicil <luicilfernandes@hotmail.com>
2021-05-18 11:45:16 -0300

    Paginação e sort ok

--- ControleFinanceiro.DAL/Interfaces/IFuncaoRepositorio.cs
+++ ControleFinanceiro.DAL/Interfaces/IFuncaoRepositorio.cs
@@ -1,7 +1,17 @@
 ﻿using System;
+using System.Threading.Tasks;
+using ControleFinanceiro.BLL.Models;
+
 namespace ControleFinanceiro.DAL.Interfaces
 {
-    public interface IFuncaoRepositorio
+    public interface IFuncaoRepositorio : IRepositorioGenerico<Funcao>
     {
+
+        Task AdicionarFuncao(Funcao funcao);
+
+        Task AtualizarFuncao(Funcao funcao);
+
+
+
     }
 }
--- ControleFinanceiro.DAL/Repositories/FuncaoRepositorio.cs
+++ ControleFinanceiro.DAL/Repositories/FuncaoRepositorio.cs
@@ -1,10 +1,50 @@
 ﻿using System;
+using System.Threading.Tasks;
+using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
 namespace ControleFinanceiro.DAL.Repositories
 {
-    public class FuncaoRepositorio
+    public class FuncaoRepositorio : RepositorioGenerico<Funcao>, IFuncaoRepositorio
     {
-        public FuncaoRepositorio()
+
+        private readonly Contexto _contexto;
+        private readonly RoleManager<Funcao> _gerenciadorFuncoes;
+
+        public FuncaoRepositorio(Contexto contexto, RoleManager<Funcao> gerenciadorFuncoes) : base(contexto) {
+            _contexto = contexto;
+            _gerenciadorFuncoes = gerenciadorFuncoes;
+        }
+
+
+        public async Task AdicionarFuncao(Funcao funcao)
         {
+            try
+            {
+                await _gerenciadorFuncoes.CreateAsync(funcao);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task AtualizarFuncao(Funcao funcao)
+        {
+            try
+            {
+                Funcao f = await PegarPeloID(funcao.Id);
+                f.Name = funcao.Name;
+                f.NormalizedName = funcao.NormalizedName;
+                f.Descricao = funcao.Descricao;
+
+                await _gerenciadorFuncoes.UpdateAsync(f);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
