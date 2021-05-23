a1eee3f519ee122728fe5596001426e46c666712
Luicil <luicilfernandes@hotmail.com>
2021-05-23 08:33:46 -0300

    Token

---
+++ ControleFinanceiro.API/ControleFinanceiro.API/Settings.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace ControleFinanceiro.API
+{
+    public static class Settings
+    {
+        public static string ChaveSecreta = Guid.NewGuid().ToString();
+    }
+}
--- ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
+++ ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 using ControleFinanceiro.API.Extentions;
 using ControleFinanceiro.API.Validacoes;
@@ -13,6 +14,7 @@ using ControleFinanceiro.DAL.Interfaces;
 using ControleFinanceiro.DAL.Repositories;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +23,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ControleFinanceiro.API
 {
@@ -66,6 +69,25 @@ namespace ControleFinanceiro.API
                 diretorio.RootPath = "ControleFinanceiro-UI";
             });
 
+            var key = Encoding.ASCII.GetBytes(Settings.ChaveSecreta);
+
+            services.AddAuthentication(opcoes =>
+            {
+                opcoes.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opcoes.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opcoes =>
+            {
+                opcoes.RequireHttpsMetadata = false;
+                opcoes.SaveToken = true;
+                opcoes.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
             services.AddControllers()
                 .AddFluentValidation()
                 .AddJsonOptions(opcoes =>
@@ -92,6 +114,8 @@ namespace ControleFinanceiro.API
             });
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
