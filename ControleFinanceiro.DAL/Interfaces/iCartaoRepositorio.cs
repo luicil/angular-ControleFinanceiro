b6283d5853a26a5fcbdd9570d2381bcfc7d1d4c9
Luicil <luicilfernandes@hotmail.com>
2021-06-14 13:43:54 -0300

    ICartaoRepositorio

--- ControleFinanceiro.DAL/Interfaces/iCartaoRepositorio.cs
+++ ControleFinanceiro.DAL/Interfaces/iCartaoRepositorio.cs
@@ -1,7 +1,19 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleFinanceiro.BLL.Models;
+using System.Text;
+using System.Collections.Generic;
+
+
 namespace ControleFinanceiro.DAL.Interfaces
 {
-    public interface iCartaoRepositorio
+    public interface ICartaoRepositorio : IRepositorioGenerico<Cartao>
     {
+        IQueryable<Cartao> PegarCartoesPeloUsuarioId(string usuarioId);
+
+        IQueryable<Cartao> FiltrarCartoes(string numeroCartao);
+
+        Task<int> PegarQuantidadeCartoesPeloUsuarioId(string usuarioId);
     }
 }
--- ControleFinanceiro.DAL/Repositories/CartaoRepositorio.cs
+++ ControleFinanceiro.DAL/Repositories/CartaoRepositorio.cs
@@ -1,10 +1,60 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
 namespace ControleFinanceiro.DAL.Repositories
 {
-    public class CartaoRepositorio
+    public class CartaoRepositorio : RepositorioGenerico<Cartao>, ICartaoRepositorio
     {
-        public CartaoRepositorio()
+        private readonly Contexto _contexto;
+        public CartaoRepositorio(Contexto contexto) : base(contexto)
         {
+            _contexto = contexto;
+        }
+
+        public IQueryable<Cartao> FiltrarCartoes(string numeroCartao)
+        {
+            try
+            {
+                return _contexto.Cartoes.Where(c => c.Numero.Contains(numeroCartao));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IQueryable<Cartao> PegarCartoesPeloUsuarioId(string usuarioId)
+        {
+            try
+            {
+                return _contexto.Cartoes.Where(c => c.UsuarioID == usuarioId);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<int> PegarQuantidadeCartoesPeloUsuarioId(string usuarioId)
+        {
+            try
+            {
+                return await _contexto.Cartoes.CountAsync(c => c.UsuarioID == usuarioId);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
